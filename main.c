66422d534483f64520d77b6a85fbb2507850344f
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-05-12 12:04:40 +0700

    Removed cherry-picked func. Added null handling. Added test case with null

--- main.c
+++ main.c
@@ -4,100 +4,6 @@
 #define matLim 1000
 #define int_max 2147483647
 #define int_min -2147483648
-//Eror codes:
-//0 = success
-//1 = bad rows value
-//2 = alloc error
-//3 = bad cols
-//4 = bad minVal
-//5 = bad maxVal
-//!(above mention) = err is null
-int** createMatrix( int rows,  int cols, long long minValue, long long maxValue, int* err) {
-    if (err == NULL) return NULL;
-    srand(time(NULL));
-    *err = 0;
-    if (rows <= 0 || rows > matLim) {
-        *err = 1;
-        return NULL;
-    }
-    if (cols <= 0 || cols > matLim) {
-        *err = 3;
-        return NULL;
-    }
-
-    if (minValue > maxValue) {
-        long long temp = minValue;
-        minValue = maxValue;
-        maxValue = temp;
-    }
-    if (maxValue > int_max || maxValue < int_min) {
-        *err = 5;
-        return NULL;
-    }
-    if (minValue > int_max || minValue < int_min) {
-        *err = 4;
-        return NULL;
-    }
-
-    int** matrix = (int**)malloc(rows * sizeof(int*));
-    if (matrix == NULL) {
-        *err = 2;
-        return NULL;
-    }
-
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int*)malloc(cols * sizeof(int));
-        if (matrix[i] == NULL) {
-            for (int j = 0; j < i; j++) {
-                free(matrix[j]);
-            }
-            free(matrix);
-            *err = 1;
-            return NULL;
-        }
-        for (int j = 0; j < cols; j++) {
-            int randValue = minValue + rand() % (maxValue - minValue + 1);
-            matrix[i][j] = randValue;
-        }
-    }
-    return matrix;
-}
-//err codes
-//1= matrix null
-//2=rows value is invalid
-//0=success
-int freeMatrix(int** matrix, int rows) {
-    if (matrix == NULL) {
-        return 1;
-    }
-    if(rows < 1 || rows > matLim) return 2;
-
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i] != NULL) {
-            free(matrix[i]);
-        }
-    }
-
-    free(matrix);
-    return 0;
-}
-//error codes:
-//1=parametr is null
-//2=too big or too small rows/cols parametr
-int printMatrix(int** matrix, int rows, int cols) {
-    if (matrix == NULL) return 1;
-    if (rows > matLim || cols > matLim) return 2;
-    if (rows < 1 || cols < 1) return 2;
-    printf("\nMatrix %dx%d:\n", rows, cols);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
 //err codes:
 // 1 = matrix is null
 //2=too big or too small rows/cols parametr
@@ -124,13 +30,14 @@ int** transponse(int** A, int row, int col, int* err) {
         return NULL;
     }
     for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            newMat[j][i] = A[i][j];
+        if (A[i]) {
+            for (int j = 0; j < col; j++) {
+                newMat[j][i] = A[i][j];
+            }
         }
     }
     freeMatrix(A, row);
     return newMat;
-
 }
 int main()
 {
@@ -139,13 +46,23 @@ int main()
     int minValue = 0;
     int maxValue = 1;
     int err;
-    // int** matrix = createMatrix(rows, cols, minValue, maxValue, &err);
+     int** matrix = createMatrix(rows, cols, minValue, maxValue, &err);
+     if (err == 0) {
+         int errPrint = printMatrix(matrix, rows, cols);
+         if (errPrint != 0) printf("Print error");
+     }
+     else printf("createMatrix error");
+     err = -1;
+
+    //null in matrix
+    //free(matrix[2]);
+    //matrix[2] = NULL;
+    // matrix = transponse(matrix, 4, 3, &err);
     // if (err == 0) {
-    //     int errPrint = printMatrix(matrix, rows, cols);
-    //     if (errPrint != 0) printf("Print error");
+    //     printMatrix(matrix, 3, 4);
     // }
-    // else printf("createMatrix error");
-    // err = -1;
+    // else printf("transponse error");
+
 
     // correct transponse
     // matrix = transponse(matrix, 4, 3, &err);
@@ -190,18 +107,18 @@ int main()
     // else printf("transponse error");
 
     //big transponse
-    int** matrix = createMatrix(100, 65, minValue, maxValue, &err);
-    if (err == 0) {
-        int errPrint = printMatrix(matrix, 100, 65);
-        if (errPrint != 0) printf("Print error");
-    }
-    else printf("createMatrix error");
-    err = -1;
-    matrix = transponse(matrix, 100, 65, &err);
-    if (err == 0) {
-        printMatrix(matrix, 65, 100);
-    }
-    else printf("transponse error");
+    //int** matrix = createMatrix(100, 65, minValue, maxValue, &err);
+    //if (err == 0) {
+    //    int errPrint = printMatrix(matrix, 100, 65);
+    //    if (errPrint != 0) printf("Print error");
+    //}
+    //else printf("createMatrix error");
+    //err = -1;
+    //matrix = transponse(matrix, 100, 65, &err);
+    //if (err == 0) {
+    //    printMatrix(matrix, 65, 100);
+    //}
+    //else printf("transponse error");
 
     return 0;
 }
